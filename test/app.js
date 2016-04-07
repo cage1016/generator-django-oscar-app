a50a3b02a5e4034001e6c6264225febe2d277a15
Kai-Chu Chung <cage.chung@gmail.com>
2016-04-07 18:21:35 +0800

    modify test

--- test/app.js
+++ test/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-var getDefaultFilesForPackagePath = function (packagename) {
-  return [
+var getDefaultFilesForPackagePath = function (packagename, isPaymentPackage) {
+  var baseFiles = [
     packagename + '/__init__.py',
     packagename + '/abstract_models.py',
     packagename + '/admin.py',
@@ -53,10 +53,23 @@ var getDefaultFilesForPackagePath = function (packagename) {
     'tests/settings.py',
     'tests/unit/__init__.py',
   ];
+
+  var isPaymentPackageFiles = [
+    packagename + '/gateway.py',
+    packagename + '/facade.py',
+    packagename + '/templates/' + packagename + '/preview.html',
+    'sandbox/apps/checkout/__init__.py',
+    'sandbox/apps/checkout/app.py',
+    'sandbox/apps/checkout/config.py',
+    'sandbox/apps/checkout/models.py',
+    'sandbox/apps/checkout/views.py',
+    'sandbox/templates/checkout/payment_details.html',
+  ];
+
+  return isPaymentPackage ? baseFiles.concat(isPaymentPackageFiles) : baseFiles;
 };
 
-
-describe('generator-django-oscar-app:app', function () {
+describe('generator-django-oscar-app general package', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
       .withPrompts({
@@ -67,12 +80,13 @@ describe('generator-django-oscar-app:app', function () {
         description: 'to be done',
         author: 'Kai-Chu Chung <cage.chung@gmail.com>',
         packagename: 'hooks',
+        isPaymentPackage: false
       })
       .on('end', done);
   });
 
   it('creates hooks package files', function () {
-    assert.file(getDefaultFilesForPackagePath('hooks'));
+    assert.file(getDefaultFilesForPackagePath('hooks', false));
   });
 
   it('generates prompts in every file', function () {
@@ -82,6 +96,39 @@ describe('generator-django-oscar-app:app', function () {
     assert.fileContent('setup.py', 'description=(\'to be done\')');
     assert.fileContent('setup.py', 'keywords=\'Oscar,django,hooks\'');
     assert.fileContent('hooks/version.py', '__version__ = \'0.1.0\'  # NOQA');
+  });
+
+});
+
+describe('generator-django-oscar-app payment package', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        user: 'cage1016',
+        repo: 'django-oscar-hooks',
+        version: '0.1.0',
+        keywords: 'django,hooks',
+        description: 'to be done',
+        author: 'Kai-Chu Chung <cage.chung@gmail.com>',
+        packagename: 'hooks',
+        isPaymentPackage: true
+      })
+      .on('end', done);
+  });
+
+  it('creates hooks payment package files', function () {
+    assert.file(getDefaultFilesForPackagePath('hooks', true));
+  });
+
+  it('generates prompts in every file', function () {
+    assert.fileContent('setup.py', 'name=\'django-oscar-hooks\'');
+    assert.fileContent('setup.py', 'url=\'https://github.com/cage1016/django-oscar-hooks.git\'');
+    assert.fileContent('setup.py', 'author=\'Kai-Chu Chung <cage.chung@gmail.com>\'');
+    assert.fileContent('setup.py', 'description=(\'to be done\')');
+    assert.fileContent('setup.py', 'keywords=\'Oscar,django,hooks\'');
+    assert.fileContent('hooks/version.py', '__version__ = \'0.1.0\'  # NOQA');
+    assert.fileContent('sandbox/templates/checkout/payment_details.html', 'title="{% trans "Pay with Hooks" %}"');
+    assert.fileContent('hooks/templates/hooks/preview.html', 'action="{% url \'hooks-place-order\' basket.id %}"');
   });
 
 });
